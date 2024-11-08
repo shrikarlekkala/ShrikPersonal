97f468f41b7f2a6f56c657ca7e320efbe3910dca
shrikarlekkala <90427147+shrikarlekkala@users.noreply.github.com>
2024-11-07 23:59:44 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,9 +31,6 @@ export default function Home() {
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Shrikar Lekkala
       </h1>
-      <h2 className="py-3.5 px-0.5 z-10 text-4xs text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Tech Founder | AI in Language | Empowering Global Communication
-      </h2>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
